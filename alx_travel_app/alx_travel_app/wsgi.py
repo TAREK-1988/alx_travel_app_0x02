aa73bd0d80b7cc4733b1015bc058b30d898c6091
TAREK <tareksayedmokhtar@yahoo.com>
2025-12-11 21:29:06 +0200

    Update wsgi.py

--- alx_travel_app/alx_travel_app/wsgi.py
+++ alx_travel_app/alx_travel_app/wsgi.py
@@ -1,1 +1,10 @@
+from __future__ import annotations
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_travel_app.settings")
+
+application = get_wsgi_application()
 
