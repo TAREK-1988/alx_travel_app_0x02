9b25f4441df4636717adf579facb4cde45990058
TAREK <tareksayedmokhtar@yahoo.com>
2025-12-11 21:28:45 +0200

    Update asgi.py

--- alx_travel_app/alx_travel_app/asgi.py
+++ alx_travel_app/alx_travel_app/asgi.py
@@ -1,1 +1,10 @@
+from __future__ import annotations
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_travel_app.settings")
+
+application = get_asgi_application()
 
