afd3982b8481b424bff571d061eecff382eff2ca
TAREK <tareksayedmokhtar@yahoo.com>
2026-01-06 23:51:13 +0200

    Update celery.py

--- alx_travel_app/alx_travel_app/celery.py
+++ alx_travel_app/alx_travel_app/celery.py
@@ -1,7 +1,4 @@
-from __future__ import annotations
-
 import os
-
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_travel_app.settings")
@@ -9,4 +6,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_travel_app.settings")
 app = Celery("alx_travel_app")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
-
